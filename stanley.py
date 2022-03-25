45bd08ced43a785ebc264c10706bb84784a34b78
weird#1337 <101741857+weird1337@users.noreply.github.com>
2022-03-25 21:41:59 +0000

    Update stanley.py

--- stanley.py
+++ stanley.py
@@ -126,7 +126,7 @@ _\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'_
     clear()
 
 def si():
-    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mGhost \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to Stellar CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mGithub: SkyWtkh \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mNew Methods \x1b[38;2;0;255;255m+ \x1b[38;2;233;233;233mNew UI!')
+    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233m1337 \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to Stanley CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mGithub: SkyWtkh-weird1337 \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mNew Methods \x1b[38;2;0;255;255m+ \x1b[38;2;233;233;233mNew UI!')
     print("")
 
 def tools():
