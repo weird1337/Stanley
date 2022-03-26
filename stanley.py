48e0f79eef892ca04ff4a16d5b2c86e2ad0dd85c
weird#1337 <101741857+weird1337@users.noreply.github.com>
2022-03-26 04:54:45 +0000

    Update stanley.py

--- stanley.py
+++ stanley.py
@@ -9,7 +9,6 @@ import sys
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-os.system ("rm -rf proxies.txt && wget http://51.107.82.239/xd/src/storage/proxies.txt")
 proxys = open('proxies.txt').readlines()
 bots = len(proxys)
 
